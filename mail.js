8a361edfb6cbdd856a3a3765400d3b4ac3a6a6b9
simplon-marseille <acardnicolas@hotmail.com>
2020-02-03 17:29:51 +0100

    deploy on heroku

--- mail.js
+++ mail.js
@@ -4,15 +4,34 @@ const nodeMailer = require('nodemailer');
 
 //step 1
 let transporter = nodeMailer.createTransport({
+    pool: true,
     service: 'gmail',
-    host: 'smtp.gmail.com',
-    secure: true,
-    port: 465,
     auth: {
+        type: 'OAuth2',
         user: process.env.EMAIL,
-        pass: process.env.PASSWORD
+        pass: process.env.PASSWORD,
+        accessToken: 'ya29.Il-8B_Wl3TIpXdVEJYD31uKLmt_S1zSVqYcyXi-Ep2NLj5s26DReaoyYY-_qw197YrI1Lamwn6YHG7dKV0Sun79MDYKvDkw8H_IswKym9JUlY8R4Qr-kOX9zKLIruO1YRg',
+        expires: 1234543635634524514 + 6000000,
+        refreshToken: process.env.EMAIL_REFRESH_TOKEN,
+        clientId: process.env.EMAIL_CLIENT_ID,
+        clientSecret: process.env.EMAIL_CLIENT_SECRET,
+        accessUrl: 'https://oauth2.googleapis.com/token'
     }
 });
+
+transporter.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our messages");
+        }
+        transporter.on('token', token => {
+            console.log('A new access token was generated');
+            console.log('User: %s', token.user);
+            console.log('Access Token: %s', token.accessToken);
+            console.log('Expires: %s', new Date(token.expires));
+        });
+      });
 
 const sendMail = (subject, email, text) => {
     //step2
@@ -25,7 +44,7 @@ const sendMail = (subject, email, text) => {
         text: textBody,
         html: htmlBody
     };
-
+    
     //step 3
     transporter.sendMail(mailOptions, (err, data) => {
         if(err){
--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.post('/email', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('App listen on port' + port);
+    console.log(`App listen at http://localhost:${port}`);
 })
