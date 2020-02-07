347cee97a2a6e0e6098328edcf8a09c20dc2cd7d
simplon-marseille <acardnicolas@hotmail.com>
2020-02-07 18:50:03 +0100

    mobile setting 2

--- public/formData.js
+++ public/formData.js
@@ -2,27 +2,42 @@ const sujet = document.querySelector('#sujet');
 const email = document.querySelector('#email');
 const texte = document.querySelector('#text');
 const button = document.querySelector('#submit');
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal-footer button');
+
+modalButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.remove('show');
+});
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
     const data = {
-        s: sujet.value,
+        // s: sujet.value,
         e: email.value,
         t: texte.value
     }
-    console.log(data);
-    fetch('/email', {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-          },
-        body: JSON.stringify({ data })
-    })
-    .then(response => response.json())
-    .then((dataFromServer) => {
-        console.log('Donnée reçus: ' + dataFromServer);
-    })
-    .catch((err) => {
-        console.log('Mon erreur' + err);
-    })
+
+    if(data.t.length === 0 || data.e.length === 0){
+        // alert('Vous navez pas remplis un des formulaire');
+         modal.classList.add('show');
+        modal.style.display = 'block';
+    }else{
+        button.setAttribute('disabled', '');
+        console.log(data);
+        fetch('/email', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+              },
+            body: JSON.stringify({ data })
+        })
+        .then(response => response.json())
+        .then((dataFromServer) => {
+            console.log('Donnée reçus: ' + dataFromServer);
+        })
+        .catch((err) => {
+            console.log('Mon erreur' + err);
+        })
+    }  
 });
--- public/navbar.js
+++ public/navbar.js
@@ -1,10 +1,72 @@
 const nav = document.querySelector('nav');
+const totop = document.querySelector('.totop');
+const toBottom = document.querySelector('.header-button .contact');
+const toBottom2 = document.querySelector('.contact');
+const body = document.querySelector('body');
+const card = document.querySelector('.wrapper-card');
+const toFormation = document.querySelector('.header-button .b1');
+const arrayOfLink = document.querySelectorAll('.dropdown-menu a');
+const arrayOfCard = document.querySelectorAll('.card-body a');
 
 
 window.addEventListener('scroll', (event) => {
     if(window.scrollY > 50){
         nav.classList.add('nav-scroll');
+        totop.style.display = 'block';
     }else{
         nav.classList.remove('nav-scroll');
+        totop.style.display = 'none';
     }
 });
+
+totop.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+});
+
+toBottom.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({
+        top: body.scrollHeight,
+        behavior: 'smooth'
+    });
+});
+
+toBottom2.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({
+        top: body.scrollHeight,
+        behavior: 'smooth'
+    });
+});
+
+arrayOfLink.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        card.scrollIntoView({behavior: "smooth"});
+    });
+});
+
+toFormation.addEventListener('click', (event) => {
+    event.preventDefault();
+    card.scrollIntoView({behavior: "smooth"});
+});
+
+arrayOfCard.forEach((card) => {
+    card.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.scrollTo({
+            top: body.scrollHeight,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+//-------------------------------------------
+
+
